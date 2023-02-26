2f71ee71b0903aa9c238ce29818887a83b4e8e85
rr5050 <11246147+rr5050@users.noreply.github.com>
2023-02-26 19:43:55 +0100

    logger created (#1)

---
+++ src/service/logger.service.js
@@ -0,0 +1,43 @@
+'use strict'
+import * as dotenv from 'dotenv'
+import log from 'simple-node-logger'
+import { isStringAndMinLength } from '../utils/misc.utils.js'
+
+/*
+	import:
+		import { logger } from './service/logger.service.js'
+
+	Usage:
+		logger.trace('<message>')
+		logger.debug('<message>')
+		logger.info('<message>')
+		logger.warn('<message>')
+		logger.error('<message>')
+		logger.fatal('<message>')
+*/
+
+dotenv.config()
+
+const logOptions = {
+	logFilePath: process.env.LOG_PATHFILE,
+	timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
+}
+
+// remove logging to file if a pathfile was not provided in environemt variables
+if (!isStringAndMinLength(process.env.LOG_PATHFILE)) {
+	delete logOptions.logFilePath
+}
+
+export const logger = log.createSimpleLogger(logOptions)
+
+/*
+	Log levels: (setting : what is logged) (if not set, 'info' will be used) (set in environment variables)
+	'all'   : all
+	'trace' : all
+	'debug' : debug, info, warn, error, fatal
+	'info'  : info, warn, error, fatal
+	'warn'  : warn, error, fatal
+	'error' : error, fatal
+	'fatal' : fatal
+*/
+logger.setLevel(process.env.LOG_LEVEL)
